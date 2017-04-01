dfcb70a916cb2343e06dfb71f4c9344cc4516b01
vegan13 <vegan13@o2.pl>
2017-04-01 13:54:32 +0200

    Better Kanban

---
+++ js/App.js
@@ -0,0 +1,65 @@
+// OGÓLNA FUNKCJA
+function randomString() {
+	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
+	var str = '', i;
+	for (i = 0; i < 10; i++) {
+	  str += chars[Math.floor(Math.random() * chars.length)];
+	}
+	return str;
+}
+
+// Funkcja setupColumns
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);        
+    });
+}
+
+// Funkcja setupCards
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(card);
+  	})
+}
+
+// Komunikacja z serwerem
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+    'X-Client-Id': <[X-Client-Id]>,
+    'X-Auth-Token': <[X-Auth-Token]>
+};
+
+$.ajaxSetup({
+    headers: myHeaders
+});
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+// TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
+var todoColumn = new Column('Do zrobienia');
+var doingColumn = new Column('W trakcie');
+var doneColumn = new Column('Skończone');
+
+// DODAWANIE KOLUMN DO TABLICY
+board.createColumn(todoColumn);
+board.createColumn(doingColumn);
+board.createColumn(doneColumn);
+
+// TWORZENIE NOWYCH EGZEMPLARZY KART
+var card1 = new Card('Nowe zadanie');
+var card2 = new Card('stworzyc tablice kanban');
+
+// DODAWANIE KART DO KOLUMN
+todoColumn.createCard(card1);
+doingColumn.createCard(card2);
